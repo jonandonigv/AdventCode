28bc3f06f855ab4e574cfe26966871ca3a053b8e
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-12-01 23:50:35 +0100

    it works

--- 2023/day1/dayOne.ts
+++ 2023/day1/dayOne.ts
@@ -11,25 +11,14 @@ const data = fs.readFile("data.txt", "utf-8", (err, result) => {
     let numberArray = [];
     let arrayD = [];
     array.forEach((str) => {
-      const num = str.match(/[0-9]/g);
+      const num = str.match(/[0-9]/g)
 
       let len = num?.length
 
-      // console.log(num?.length, num)
-
-      /* if (num?.length === 1) {
-        const n = num + num;
-        arrayD.push(n);
-      } */
+      //console.log(num?.length, num)
 
       const n2 = num[0] + num[len - 1]
       arrayD.push(n2)
-
-      /* if (num?.length >= 2) {
-        let len = num?.length;
-        const n2 = num[0] + num[len - 1];
-        array.push(n2)
-      } */
     });
 
     console.log(arrayD);
